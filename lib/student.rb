398b45a0706e6fdd11aec75f0590198894bb07a1
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-15 01:02:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,10 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE name = ?
+      LIMIT 1
     SQL
   end
   
